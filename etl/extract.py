58688cce9c311086e618d9c2d9a69d2ef67ffe99
mwaimichael <michael.k.mwai@gmail.com>
2026-04-20 01:08:08 +0300

    Fix .gitignore rules

--- etl/extract.py
+++ etl/extract.py
@@ -2,15 +2,14 @@ import requests
 from config import API_KEY
 from pprint import pprint
 
+url = f"https://api.binance.com/api/v3/ticker/24hr"
+
+headers = {
+    'X-MBX-APIKEY': API_KEY
+}
 
 def get_crypto_price():
-
-    url = f"https://api.binance.com/api/v3/ticker/24hr"
     
-    
-    headers = {
-        'X-MBX-APIKEY': API_KEY
-    }
     try:
         data = requests.get(url, headers=headers).json()
     except Exception as e:
@@ -18,38 +17,4 @@ def get_crypto_price():
 
 
     return data
-
-
-
-
-# Usage
-'''MY_API_KEY = "Y4XPiIHHu1ujSaDFF0uYAL6yJG8YPFLoQGsGIaWWUaIL9eOjaYU227YAlF4AwsyL"
-btc_price = get_crypto_price("BTCUSDT", MY_API_KEY)
-#pprint(f"Current BTC Price: {btc_price}")
-print(len(btc_price))'''
-
-
-'''prices = []
-
-for each in btc_price:
-    price = {
-        "crypto": each.get("symbol"),
-        "price": each.get("weightedAvgPrice")
-    }
-    prices.append(price)
-
-#print(prices, sep="\n")
-
-df = pd.DataFrame(prices)
-df['price'] = df['price'].astype(float).round(2)
-pd.set_option('display.float_format', '{:,.2f}'.format)
-df = df[df['crypto'].isin(['BTCUSDT', 'WBTCUSDT', 'USDTIDR', 'USDTBIDR', 'USDTIDRT', 'XAUTUSDT', 'PAXGUSDT', 'USDTCOP', 'YFIUSDT', 'WBETHUSDT'])]
-#df = df[df['crypto'].isin(['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'MKRUSDT', 'TAOUSDT', 'LTCUSDT', 'QNTUSDT', 'YFIBUSD', 'XAUTUSDT', 'BNBUSDT'])]
-df.sort_values(by='price', ascending=False, inplace=True)
-
-#df.info(verbose=False)
-print("-"*20)
-
-print(df.head(10))
-'''
 
